63c3bcbcafadb734ebbf423f215e1eaaee68d478
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2021-09-15 18:00:14 +0530

    verson 0.2

--- rat_game.py
+++ rat_game.py
@@ -3,30 +3,35 @@ import threading
 import time
 import keyboard as key
 
-
+#prints rat at a random time and starts a one second timer for the user to react
 def rat():
     rat.a=0
     rat.c=0
-    rat.score=0
     for i in range(1,100):
         rat.a=random.randint(1,100)
         if int(rat.a)%2==0:
             rat.b=time.sleep(random.randint(1,5))
             print("rat")
             rat.c="rat"
-            timer=threading.Timer(2.0,keyboard)
+            timer=threading.Timer(1.0,keyboard)
             timer.start()
             break
+        
+#Universal variable
+score=0
+
+#checks if the user is pressing the esc key while rat is printed and updates the score 
 def keyboard():
+    global score
     keyboard.d=key.is_pressed('esc')
     if rat.c=="rat" and keyboard.d==True:
-        print("you caught the rat")
-        rat.score=rat.score+1
-        print(f"your score is: ",rat.score)
+        print("you caught the rat!")
+        score=score+1
+        print(f"your score is: ",score)
         rat()
     else:
-        print(f"you missed the rat")
-        print(f"your score is: ",rat.score)
+        print(f"you missed the rat!")
+        print(f"your score is: ",score)
                     
 #_main_        
 rat() 
